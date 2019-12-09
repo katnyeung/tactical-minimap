7ac098d127c8eb509a6bcee182e3f69ee1ac9f82
Wing Chung <katnyeung@gmail.com>
2019-12-09 22:13:18 +0800

    build first before district

--- src/main/java/org/tactical/minimap/scheduler/TelegramParserScheduler.java
+++ src/main/java/org/tactical/minimap/scheduler/TelegramParserScheduler.java
@@ -191,9 +191,9 @@ public class TelegramParserScheduler {
 
 					message = telegramMessageService.processData(message, "street", keyMap, 50);
 
-					message = telegramMessageService.processData(message, "district", keyMap, 25);
-
 					message = telegramMessageService.processData(message, "building", keyMap, 15);
+					
+					message = telegramMessageService.processData(message, "district", keyMap, 25);
 
 					message = telegramMessageService.processData(message, "plaza", keyMap, 15);
 
