e50f1f13b04198726d2ea31a8f6269b9468b57f3
Wing Chung <katnyeung@gmail.com>
2020-01-12 13:36:07 +0800

    no more random tg marker result when grouping active

--- src/main/java/org/tactical/minimap/scheduler/TelegramParserScheduler.java
+++ src/main/java/org/tactical/minimap/scheduler/TelegramParserScheduler.java
@@ -454,11 +454,11 @@ public class TelegramParserScheduler {
 
 		logger.info("adding marker " + marker.getType());
 
-		double randLat = (ThreadLocalRandom.current().nextInt(0, 80 + 1) - 40) / 100000.0;
-		double randLng = (ThreadLocalRandom.current().nextInt(0, 80 + 1) - 40) / 100000.0;
+		//double randLat = (ThreadLocalRandom.current().nextInt(0, 80 + 1) - 40) / 100000.0;
+		//double randLng = (ThreadLocalRandom.current().nextInt(0, 80 + 1) - 40) / 100000.0;
 
-		markerDTO.setLat(markerDTO.getLat() + randLat);
-		markerDTO.setLng(markerDTO.getLng() + randLng);
+		markerDTO.setLat(markerDTO.getLat());
+		markerDTO.setLng(markerDTO.getLng());
 		
 		markerService.addMarker(layer, markerDTO, marker);
 	}
