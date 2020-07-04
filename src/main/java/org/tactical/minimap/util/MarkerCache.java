3bdc032e231971ba066c42a8b72e77a5ae8ba11f
Wing Chung <katnyeung@gmail.com>
2020-07-04 21:04:51 +0800

    hide markerId from markerCache

--- src/main/java/org/tactical/minimap/util/MarkerCache.java
+++ src/main/java/org/tactical/minimap/util/MarkerCache.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class MarkerCache {
+	@JsonIgnore
 	Long markerId;
 
 	@JsonIgnore
